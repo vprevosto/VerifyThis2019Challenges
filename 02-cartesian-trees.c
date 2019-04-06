4cfecb4e0304108bba0f4422bda377c4021b938d
Virgile Prevosto <virgile.prevosto@m4x.org>
2019-04-06 14:56:24 +0200

    improvements

--- 02-cartesian-trees.c
+++ 02-cartesian-trees.c
@@ -3,9 +3,10 @@
 /*@ requires \valid(s + (0 .. length - 1));
     requires \valid(stack + (0 .. length - 1));
     requires \valid(left + (0 .. length - 1));
+    requires \separated(stack + (0 .. length - 1),left + (0 .. length - 1));
     assigns stack[0 .. length - 1], left[0 .. length - 1];
 
-    ensures wf_left: \forall integer i; 0 <= i < length ==> left[i] < i;
+    ensures wf_left: \forall integer i; 0 <= i < length ==> left[i] <= i;
     ensures left_small:
     \forall integer i; 0 <= i < length ==>
      left[i] > 0 ==> s[left[i]-1] < s[i];
@@ -20,7 +21,7 @@ void neighbor(int* s, size_t length, size_t* stack, size_t* left) {
       loop invariant stack_left:
         \forall integer i; 0 <= i < sidx ==> 0 < stack[i] <= x;
       loop invariant wf_left:
-        \forall integer i; 0 <= i < x ==> left[i] < i;
+        \forall integer i; 0 <= i < x ==> left[i] <= i;
       loop invariant left_small:
         \forall integer i; 0 <= i < x ==> s[left[i] - 1] < s[i];
       loop invariant left_smallest:
