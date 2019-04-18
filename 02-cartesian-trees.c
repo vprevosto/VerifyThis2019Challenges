0ff5812f9073da95a6cd00bb04aed8e059bb8ec2
Virgile Prevosto <virgile.prevosto@m4x.org>
2019-04-18 10:40:57 +0200

    Correct invariants seem easier to prove

--- 02-cartesian-trees.c
+++ 02-cartesian-trees.c
@@ -9,7 +9,7 @@
 
     assigns stack[0 .. length - 1], left[0 .. length - 1];
 
-    ensures wf_left: \forall integer i; 0 <= i < length ==> left[i] <= i;
+    ensures wf_left: \forall integer i; 0 <= i < length ==> 0 <= left[i] <= i;
     ensures left_small:
     \forall integer i; 0 <= i < length ==> 0 < left[i] ==>
      left[i] > 0 ==> s[left[i]-1] < s[i];
@@ -19,28 +19,32 @@
 */
 void neighbor(int* s, size_t length, size_t* stack, size_t* left) {
   size_t sidx = 0;
-  /*@ loop invariant 0 <= x <= length;
+  /*@ loop invariant s_untouched:
+        \forall integer idx; 0 <= idx < length ==> s[idx] == \at(s[idx],Pre);
+      loop invariant 0 <= x <= length;
       loop invariant 0 <= sidx <= x;
       loop invariant stack_left:
         \forall integer i; 0 <= i < sidx ==> 0 < stack[i] <= x;
       loop invariant wf_left:
-        \forall integer i; 0 <= i < x ==> left[i] <= i;
+        \forall integer i; 0 <= i < x ==> 0 <= left[i] <= i;
       loop invariant left_small:
         \forall integer i; 0 <= i < x ==> left[i] > 0 ==> s[left[i] - 1] < s[i];
       loop invariant left_smallest:
         \forall integer i; 0 <= i < x ==>
           \forall integer j; left[i] <= j < i ==> s[j] >= s[i];
       loop invariant stack_order:
-        \forall integer i, j; 0<= i < j < sidx ==> stack[i] < stack[j];
+        \forall integer i, j; 0<= i < j < sidx ==> 0 <= stack[i] < stack[j];
       loop invariant stack_sorder:
         \forall integer i, j;
           0<= i < j < sidx ==> s[stack[i]-1] < s[stack[j]-1];
       loop invariant s_begin:
       sidx > 0 ==>
         \forall integer i; 0<=i<stack[0] ==> s[i] >= s[stack[0] - 1];
+      loop invariant step_n:
+        x > 0 ==> sidx > 0 && stack[sidx - 1] == x;
       loop invariant stack_summary:
         \forall integer i; 0<= i < sidx - 1 ==>
-          \forall integer j; stack[i]-1< j < stack[i+1]-1 ==>
+          \forall integer j; stack[i] <= j < stack[i+1]-1 ==>
                  s[j] >= s[stack[i+1]-1];
       loop invariant stack_push: sidx > 0 ==> stack[sidx-1] == x;
       loop assigns x, sidx, stack[0 .. length - 1], left[0 .. length - 1];
@@ -48,10 +52,16 @@ void neighbor(int* s, size_t length, size_t* stack, size_t* left) {
    */
   for (size_t x = 0; x < length; x++) {
     /*@
+       loop invariant s_untouched_inner:
+        \forall integer idx; 0 <= idx < length ==> s[idx] == \at(s[idx],Pre);
         loop invariant 0 <= sidx <= \at(sidx,LoopEntry);
-        loop invariant stack:
+        loop invariant left_bigger:
+          sidx > 0 ==>
           \forall integer i;
-             sidx <= i < \at(sidx,LoopEntry) ==> s[stack[i]-1] >= s[x];
+             stack[sidx-1] <= i < x ==> s[i] >= s[x];
+        loop invariant stack_empty:
+          sidx == 0 ==>
+          \forall integer i; 0 <= i < x ==> s[i] >= s[x];
         loop assigns sidx;
         loop variant sidx;
      */
@@ -59,13 +69,19 @@ void neighbor(int* s, size_t length, size_t* stack, size_t* left) {
     if (sidx == 0) {
       left[x] = 0;
     } else {
+      /*@ assert head_ok:
+            \forall integer i; stack[sidx-1]<=i<x ==> s[i] >= s[x];
+      */
       left[x] = stack[sidx - 1];
     }
 	//@ assert a1: left[x] > 0 ==> s[left[x] - 1] < s[x];
 label:
     stack[sidx] = x + 1;
-	//@ assert same: left[x] == \at(left[x], label);
-	//@ assert a2: left[x] > 0 ==> s[left[x] - 1] < s[x];
+    /*@ assert s_untouched:
+        \forall integer idx; 0 <= idx < length ==> s[idx] == \at(s[idx],Pre);
+    */
+    //@ assert same: left[x] == \at(left[x], label);
+    //@ assert a2: left[x] > 0 ==> s[left[x] - 1] < s[x];
     sidx++;
   }
 }
